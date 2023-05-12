58303e732631c0acb7b899ee74b009967e9b26fb
Laila Case <lailacase@Lailas-MacBook-Pro.local>
2023-05-12 14:25:10 +0100

    task 7 started

--- models/comments.models.js
+++ models/comments.models.js
@@ -15,3 +15,23 @@ exports.fetchCommentsByReviewID = (reviewId) => {
       return comments.rows;
     });
 };
+
+exports.createCommentByReviewId = (reviewId, comment) => {
+  return db
+  .query(`SELECT * FROM reviews WHERE review_id = $1`, [
+    reviewId,
+  ])
+  .then((review) => {
+    if (review.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "404 - no such review ID found, please use an existing review ID to post a comment"
+      });
+    }
+    const { author, body } = comment
+    return db
+    .query(`
+    INSERT INTO comments
+    (author, body)
+    VALUES`)
+})}
